f67f4229083a473edbaf12b9cb0572cf3de70d41
Акавов Расул <vavako57@gmail.com>
2024-12-10 00:31:54 +0300

    Добавьте файлы проекта.

---
+++ CourseWorkApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseWorkApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
