c283782ca4ba0b597cd36b9913d668516231d8b9
Акавов Расул <vavako57@gmail.com>
2024-12-17 01:56:05 +0300

    Рефакторинг и добавление кода в Program.cs.

--- CourseWorkApp/Program.cs
+++ CourseWorkApp/Program.cs
@@ -13,37 +13,37 @@
             MainMenu();
         }
 
-        static void LoadData()
+        static void LoadAmbulanceCalls()
         {
-            if (File.Exists(AmbulanceCallsFilePath))
-            {
-                var allAmbulanceCallsLines = File.ReadAllLines(AmbulanceCallsFilePath);
+            var allAmbulanceCallsLines = File.ReadAllLines(AmbulanceCallsFilePath);
 
-                foreach (var ambulanceCallLine in allAmbulanceCallsLines)
-                {
-                    var splitAmbulanceCallLine = ambulanceCallLine.Split(";");
-                    AmbulanceCall ambulanceCall;
-                    ambulanceCall.BrigadeNumber = int.Parse(splitAmbulanceCallLine[0]);
-                    ambulanceCall.BrigadeSpecialization = splitAmbulanceCallLine[1];
-                    ambulanceCall.CallDate = DateTime.Parse(splitAmbulanceCallLine[2]);
-                    ambulanceCall.CallTime = TimeSpan.Parse(splitAmbulanceCallLine[3]);
-                    ambulanceCall.Address = splitAmbulanceCallLine[4];
-                    ambulanceCall.PatientFullName = splitAmbulanceCallLine[5];
-                    ambulanceCall.PatientBirthDate = DateTime.Parse(splitAmbulanceCallLine[6]);
-                    ambulanceCall.PatientSex = splitAmbulanceCallLine[7];
-                    ambulanceCall.BrigadeActions = splitAmbulanceCallLine[8];
-                }
-            }
-
-            if (File.Exists(SpecializationsOfBrigadesFilePath))
+            foreach (var ambulanceCallLine in allAmbulanceCallsLines)
             {
-                SpecializationsOfBrigades.AddRange(File.ReadAllLines(SpecializationsOfBrigadesFilePath));
-            }
+                var splitAmbulanceCallLine = ambulanceCallLine.Split(";");
 
-            if (File.Exists(ActionsOfBrigadesFilePath))
-            {
-                ActionsOfBrigades.AddRange(File.ReadAllLines(ActionsOfBrigadesFilePath));
+                AmbulanceCall ambulanceCall;
+                ambulanceCall.BrigadeNumber = int.Parse(splitAmbulanceCallLine[0]);
+                ambulanceCall.BrigadeSpecialization = splitAmbulanceCallLine[1];
+                ambulanceCall.CallDate = DateTime.Parse(splitAmbulanceCallLine[2]);
+                ambulanceCall.CallTime = TimeSpan.Parse(splitAmbulanceCallLine[3]);
+                ambulanceCall.Address = splitAmbulanceCallLine[4];
+                ambulanceCall.PatientFullName = splitAmbulanceCallLine[5];
+                ambulanceCall.PatientBirthDate = DateTime.Parse(splitAmbulanceCallLine[6]);
+                ambulanceCall.PatientSex = splitAmbulanceCallLine[7];
+                ambulanceCall.BrigadeActions = splitAmbulanceCallLine[8];
+
+                AmbulanceCalls.Add(ambulanceCall);
             }
+        }
+
+        static void LoadSpecializationsOfBrigades()
+        {
+            SpecializationsOfBrigades.AddRange(File.ReadAllLines(SpecializationsOfBrigadesFilePath));
+        }
+
+        static void LoadActionsOfBrigades()
+        {
+            ActionsOfBrigades.AddRange(File.ReadAllLines(ActionsOfBrigadesFilePath));
         }
 
         static void CallsMenu()
@@ -51,10 +51,12 @@
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("1. ");
-                Console.WriteLine("2. ");
-                Console.WriteLine("3. ");
-                Console.WriteLine("4. Назад");
+                Console.WriteLine("1. Просмотреть вызовы");
+                Console.WriteLine("2. Добавление вызова");
+                Console.WriteLine("3. Удаление вызова");
+                Console.WriteLine("4. Сортировка вызовов");
+                Console.WriteLine("5. Поиск вызовов");
+                Console.WriteLine("0. Назад");
                 Console.Write(">>> ");
 
                 var option = Console.ReadKey().Key;
@@ -68,7 +70,7 @@
                         break;
                     case ConsoleKey.D3:
                         break;
-                    case ConsoleKey.D4:
+                    case ConsoleKey.D0:
                         stop = true;
                         break;
                 }
@@ -87,7 +89,7 @@
                 Console.Clear();
                 Console.WriteLine("1. Действия бригады");
                 Console.WriteLine("2. Специализация врачей бригады");
-                Console.WriteLine("3. Назад");
+                Console.WriteLine("0. Назад");
                 Console.Write(">>> ");
 
                 var option = Console.ReadKey().Key;
@@ -99,7 +101,44 @@
                         break;
                     case ConsoleKey.D2:
                         break;
+                    case ConsoleKey.D0:
+                        stop = true;
+                        break;
+                }
+
+                if (stop)
+                {
+                    break;
+                }
+            }
+        }
+
+        static void PrintActionsOfBrigades()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Вызовы");
+                Console.WriteLine("2. Справочники");
+                Console.WriteLine("3. Обработка данных");
+                Console.WriteLine("0. Назад");
+                Console.Write(">>> ");
+
+                var option = Console.ReadKey().Key;
+                var stop = false;
+
+                switch (option)
+                {
+                    case ConsoleKey.D1:
+                        CallsMenu();
+                        break;
+                    case ConsoleKey.D2:
+                        DirectoriesMenu();
+                        break;
                     case ConsoleKey.D3:
+
+                        break;
+                    case ConsoleKey.D0:
                         stop = true;
                         break;
                 }
@@ -119,7 +158,7 @@
                 Console.WriteLine("1. Вызовы");
                 Console.WriteLine("2. Справочники");
                 Console.WriteLine("3. Обработка данных");
-                Console.WriteLine("4. Выход");
+                Console.WriteLine("0. Выход");
                 Console.Write(">>> ");
 
                 var option = Console.ReadKey().Key;
@@ -136,7 +175,7 @@
                     case ConsoleKey.D3:
 
                         break;
-                    case ConsoleKey.D4:
+                    case ConsoleKey.D0:
                         stop = true;
                         break;
                 }
